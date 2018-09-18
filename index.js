fc9015bb3499068d7b4f41393bb7d16e1636e85d
Gabriel Seijo <gabrielseijo.m@gmail.com>
2018-09-18 03:37:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 function dwarfRollCall(dwarves) {
+  var moreDwarves = [];
+  for(var i = 0; i < dwarves.length; i++) {
+    moreDwarves.push((i+1)+". "+dwarves[i] );
+  } return moreDwarves.join(' ');
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  var calls = [];
+  for(var i = 0; i < planeteerCalls.length; i++) {
+    calls.push(planeteerCalls[i].toUpperCase + "!");
+  } return calls;
 }
 
 function longPlaneteerCalls(words) {
+  if (words.length >= 4) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function findTheCheese (foods) {
+  for(var i = 0; i < foods.length; i++) {
+    if(foods[i] === "cheddar") {
+      return foods[i];
+    } 
+  }
+  if(foods !== "cheddar") {
+    return "no cheese!";
+  }
 }
