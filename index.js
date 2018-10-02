b1a78d376c579a2daf262f86b435b9798f3c383d
Gabriel Seijo <gabrielseijo.m@gmail.com>
2018-10-02 20:11:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 function dwarfRollCall(dwarves) {
   var moreDwarves = [];
   for(var i = 0; i < dwarves.length; i++) {
-<<<<<<< HEAD
     moreDwarves.push((i+1)+". "+dwarves[i] );
-=======
-    moreDwarves.push((i+1)+". "+dwarves[i]);
->>>>>>> c007e81699f66fe80689dbd570c2ab8faa3a33ca
   } return moreDwarves.join(' ');
 }
 
@@ -28,9 +24,14 @@ function findTheCheese (foods) {
   for(var i = 0; i < foods.length; i++) {
     if(foods[i] === "cheddar") {
       return foods[i];
-    } 
+    } else if(foods[i] === "swiss cheese") {
+      return foods[i];}
   }
   if(foods !== "cheddar") {
     return "no cheese!";
   }
+}
+
+function wordsWithB (words) {
+  
 }
