0acd680516fe6ec64e64b823ee56cbbebd8e2bd0
Gabriel Seijo <gabrielseijo.m@gmail.com>
2018-10-02 20:03:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 function dwarfRollCall(dwarves) {
   var moreDwarves = [];
   for(var i = 0; i < dwarves.length; i++) {
+<<<<<<< HEAD
     moreDwarves.push((i+1)+". "+dwarves[i] );
+=======
+    moreDwarves.push((i+1)+". "+dwarves[i]);
+>>>>>>> c007e81699f66fe80689dbd570c2ab8faa3a33ca
   } return moreDwarves.join(' ');
 }
 
